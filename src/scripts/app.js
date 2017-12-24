29337de6b0b36f90dd1560ca7fc532ab06241c66
Iryna Lypnyk <iryna.lypnyk@gmail.com>
2017-12-24 19:09:25 +0200

    add google.map and fix js for flipper

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,14 @@
 const flipper = require('./components/flipper.js');
+const map = require('./components/map.js');
 const $ = require('jquery');
+// import {map} from './components/map.js';
+
+const mapContainer = document.getElementById('map');
+const welcomeFlip = document.getElementById('flipper');
+
+
+if (mapContainer) {
+  google.maps.event.addDomListener(window, 'load', map.init);
+}
+
 
--- src/scripts/components/flipper.js
+++ src/scripts/components/flipper.js
@@ -3,19 +3,23 @@ var flipper=document.querySelector('.welcome__flipper');
 var toMainPageBtn=document.querySelector('.nav-btns__link_to-main-page');
 
 function toAuthForm(){
-  btn.onclick = function(){
-    btn.style.visibility = 'hidden';
-    flipper.style.transform = 'rotateY(180deg)';
-  };
+  btn.style.visibility = 'hidden';
+  flipper.style.transform = 'rotateY(180deg)';
 }
 
-function toMainPage(){
-  toMainPageBtn.onclick = function(e){
-    e.preventDefault();
-    flipper.style.transform = 'rotateY(0deg)';
-    btn.style.visibility = 'visible';
-  };
+function toMainPage(e){
+  e.preventDefault();
+  flipper.style.transform = 'rotateY(0deg)';
+  btn.style.visibility = 'visible';
 }
 
-module.exports =  toAuthForm();
-module.exports =  toMainPage();
+function toFlip(){
+  if(btn){
+    btn.addEventListener('click', toAuthForm);
+  }
+  if(toMainPageBtn){
+    toMainPageBtn.addEventListener('click', toMainPage);
+  }
+}
+
+module.exports = toFlip();
