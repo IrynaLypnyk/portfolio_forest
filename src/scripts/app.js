e9a3c5718de996c66cc49c2ae09b83da0eb4651a
Iryna Lypnyk <iryna.lypnyk@gmail.com>
2018-01-11 01:06:54 +0200

    add js for skiis-circles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,29 +8,42 @@ const burgerMenu = require('./components/burger-menu');
 const arrows = require('./components/arrows');
 const blogNav = require('./components/blog');
 const slider = require('./components/slider');
+const skills = require('./components/skills');
 
+//preloader
 if (document.images.length > 0) {
   preloader();
 
 } else {
   document.getElementById('js-preloader').classList.add('done');
-  document.body.style.overflow = 'auto';
+  // document.body.style.overflow = 'auto';
 }
 
-//blog-menu
-if (document.getElementsByClassName('blog-nav__items').length > 0) {
-  blogNav();
-}
 //flipper
 const flipperId=document.querySelector('.welcome__flipper');
 if(flipperId){
   flipper();
 }
 
+//blog-menu
+if (document.getElementsByClassName('blog-nav__items').length > 0) {
+  blogNav();
+}
+
+
 //burger
 const burgerId = document.getElementById('burger-menu');
 if (burgerId) {
   burgerMenu();
+}
+//googlemap
+const mapId = document.getElementById('map');
+if (mapId) {
+  google.maps.event.addDomListener(window, 'load', map.init);
+}
+const skillsId = document.getElementById('skills');
+if (skillsId) {
+  skills();
 }
 
 const arrowBtn = document.getElementsByClassName('header__arrow-btn');
@@ -38,13 +51,9 @@ if (arrowBtn) {
   arrows();
 }
 
-const sliderID = document.getElementById('slider');
-if (sliderID) {
+const sliderId = document.getElementById('slider');
+if (sliderId) {
   slider();
 }
 
-//googlemap
-const mapContainer = document.getElementById('map');
-if (mapContainer) {
-  google.maps.event.addDomListener(window, 'load', map.init);
-}
+
