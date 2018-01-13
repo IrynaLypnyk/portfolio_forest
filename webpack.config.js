4b599a5fc78608487dd1dc9e697474b5d08fea85
Iryna Lypnyk <iryna.lypnyk@gmail.com>
2018-01-13 17:40:50 +0200

    add jquery plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,18 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
-    output: {
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new UglifyJSPlugin({
-            sourceMap: true
-        })
-    ]
+  output: {
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true,
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
 };
 
 module.exports = config;
